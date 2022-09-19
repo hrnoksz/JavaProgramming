90e66fca0f0ee353a7edfe2116f6e3a049f028b9
hrnoksz <harun1973@yahoo.com>
2022-09-20 00:53:24 +0300

    Task Solutions MultiDimensionalArray

---
+++ taskSolutions/day22_MultiDimensionalArray/_02_ReverseTwoDimensionalArray.java
@@ -0,0 +1,23 @@
+package day22_MultiDimensionalArray;
+
+public class _02_ReverseTwoDimensionalArray {
+
+    public static void main(String[] args) {
+
+        int[][] array2D = { {1,2,3}, {4,5,6}};
+
+        for (int i = array2D.length - 1; i >= 0; i--) {
+            for (int j = array2D[i].length - 1; j >= 0; j--) {
+                System.out.print(array2D[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+1. Write a program that can reverse a two-dimensional array (return new array)
+		Ex:
+			array = { {1,2,3}, {4,5,6}};
+		output:
+			reverse = { {6,5,4}, {3,2,1},};
+ */
