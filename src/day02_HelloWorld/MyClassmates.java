029fd92ad0947b99bde2978310aa3cbe34903cb2
hrnoksz <harun1973@yahoo.com>
2022-09-20 00:03:09 +0300

    day02 Hello World

---
+++ src/day02_HelloWorld/MyClassmates.java
@@ -0,0 +1,13 @@
+package day02_HelloWorld;
+
+public class MyClassmates {
+
+    public static void main(String[] args) {
+
+        System.out.println("Osman");
+        System.out.println("Erol");
+        System.out.println("Beytullah");
+        System.out.println("Ertan");
+        System.out.println("Hakan");
+    }
+}
