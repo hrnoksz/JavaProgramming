572ead22b8a4a5a2bf3660eac73542db3976d197
hrnoksz <harun1973@yahoo.com>
2022-09-20 00:02:30 +0300

    day01 java intro

---
+++ src/day01_JavaIntro/_01_Test.java
@@ -0,0 +1,17 @@
+package day01_JavaIntro;
+
+import utilities.StringUtility;
+
+public class _01_Test {
+
+    public static void main(String[] args) {
+
+        String str = "Wooden Spoon";
+
+        StringUtility.printEachChar(str);
+
+
+
+
+    }
+}
