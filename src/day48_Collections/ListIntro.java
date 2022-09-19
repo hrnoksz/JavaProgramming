f119b785d062343be53f60c0dbfbc2958af1b80d
hrnoksz <harun1973@yahoo.com>
2022-09-20 00:37:54 +0300

    day48 Collections

---
+++ src/day48_Collections/ListIntro.java
@@ -0,0 +1,52 @@
+package day48_Collections;
+
+import java.util.*;
+
+public class ListIntro {
+
+    public static void main(String[] args) {
+
+        List<Integer> list1 = new ArrayList<>();
+        list1.addAll(Arrays.asList(10, 20, 30, 40, 50, 60, null));
+
+        System.out.println(list1);
+
+        System.out.println(list1.get(0)); //ArrayList is faster
+
+        List<Integer> list2 = new LinkedList<>();
+        list2.addAll(Arrays.asList(10, 20, 30, 40, 50, 60, null));
+        System.out.println(list2);
+
+        System.out.println(list2.get(0));
+
+        List<Integer> list3 = new Vector<>();
+        list3.addAll(Arrays.asList(10, 20, 30, 40, 50, 60, null));
+        System.out.println(list3);
+        System.out.println(list3.get(0));
+
+        List<Integer> list4 = new Stack<>();
+        list4.addAll(Arrays.asList(10, 20, 30, 40, 50, 60, null));
+        System.out.println(list4);
+        System.out.println(list4.get(0));
+
+        System.out.println("-------------------------------------------------");
+
+        System.out.println(list4); //[10, 20, 30, 40, 50, 60]
+
+        ((Stack<Integer>) list4).pop(); //Last-in, first out order (LIFO)
+
+        System.out.println(list4); //[10, 20, 30, 40, 50]
+
+        ((Stack<Integer>) list4).pop();
+
+        System.out.println(list4); //[10, 20, 30, 40]
+
+
+
+
+    }
+
+    public synchronized void method1(){
+
+    }
+}
